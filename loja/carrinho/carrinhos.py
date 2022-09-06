aab2c1cbf7fdec863959d4c1aac388f10ee6dfa2
André Bayer <andre.zbayer@hotmail.com>
2022-09-05 23:16:31 -0300

    NavBar + Carrinho (Funcionalidades)

--- loja/carrinho/carrinhos.py
+++ loja/carrinho/carrinhos.py
@@ -1,3 +1,6 @@
+
+from logging import exception
+from turtle import color
 from flask import render_template, session, request, url_for, flash, redirect, current_app
 from loja import app, db
 from loja.produtos.models import Addproduto
@@ -19,7 +22,7 @@ def AddCart():
         produto    = Addproduto.query.filter_by(id=produto_id).first()
 
         if produto_id and quantity and colors and request.method == "POST":
-            DicItems = {produto_id:{'name':produto.name, 'price':float(produto.price), 'discount':produto.discount, 'colors':produto.colors, 'quantity':produto.stock, 'image_1':produto.image_1}}
+            DicItems = {produto_id:{'name':produto.name, 'price':float(produto.price), 'discount':produto.discount, 'colors':produto.colors, 'quantity':produto.stock, 'image_1':produto.image_1, 'colors':produto.colors}}
             if 'LojainCarrinho' in session:
                 print(session['LojainCarrinho'])
                 if produto_id in session['LojainCarrinho']:
@@ -42,4 +45,41 @@ def AddCart():
 def getCart():
     if 'LojainCarrinho' not in session:
         return redirect(request.referrer)
-    return render_template('produtos/carros.html')
+    subtotal = 0
+    valorpagar = 0
+    for key, produto in session['LojainCarrinho'].items():
+        discount = (produto['discount']/100) * float(produto['price'])
+        subtotal += float(produto['price']) * int(produto['quantity'])
+        subtotal -= discount
+        imposto = ("%.2f"% (.06 * float(subtotal)))
+        valorpagar = float("%.2f" %(1.06 * subtotal))
+    return render_template('produtos/carros.html', imposto=imposto, valorpagar=valorpagar)
+
+
+@app.route('/updateCarro/<int:code>', methods=['POST'])
+def updateCarro(code):
+    if 'LojainCarrinho' not in session and len(session['LojainCarrinho']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        quantity = request.form.get('quantity')
+        color    = request.form.get('color')
+        try:
+            session.modified = True
+            for key, item in session['LojainCarrinho'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color']    = color
+                    flash('Item foi atualizado com sucesso','success')
+                    return redirect(url_for('getCart'))   
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+
+@app.route('/vazio')
+def vazio_Cart():
+    try:
+        session.clear()
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
